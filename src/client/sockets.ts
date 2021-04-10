12273c621f615b20380206055d8a599b578db11d
koreanwglasses <koreanwglasses@gmail.com>
2021-04-10 13:43:59 -0500

    wip

--- src/client/sockets.ts
+++ src/client/sockets.ts
@@ -2,5 +2,10 @@ import io from "socket.io-client";
 import { rootURL } from "../utils";
 
 const URL = rootURL();
-const socket = io(URL, { autoConnect: false });
+const socket = io(URL, { autoConnect: false }) as SocketIOClient.Socket & {
+  onAny: (handler: (event: unknown, ...args: unknown[]) => void) => void;
+};
 
+socket.onAny((event, ...args) => {
+  console.log(event, args);
+});
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const baseURL = "http://localhost";
-export const port = process.env.PORT || 9000;
+export const port = 9000;
 export const mode =
   (process.env.NODE_ENV as "production" | "development") || "production";
