e0f7252e5b21f3bb852b740c5f7d0c3e1cda96cf
koreanwglasses <koreanwglasses@gmail.com>
2021-04-10 12:47:32 -0500

    wip

--- src/server/controllers/discord.ts
+++ src/server/controllers/discord.ts
@@ -1,7 +1,7 @@
 import { Client } from "discord.js";
 import * as localConfig from "../config.local";
 
-let client = new Client();
+const client = new Client();
 
 export const start = () =>
   new Promise<void>((res) => {
