a232979a8030bf0ab32a54f2022afa45f21aa2dc
koreanwglasses <koreanwglasses@gmail.com>
2021-04-22 20:44:19 -0500

    [Feature] [WIP] Setting up remote development

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,8 @@
 export const baseURL = "http://localhost";
 export const port = 9000;
 export const mode =
-  (process.env.NODE_ENV as "production" | "development") || "production";
+  (process.env.NODE_ENV as
+    | "production"
+    | "development"
+    | "remote-development") || "production";
+export const remoteBaseURL = "http://dev.fred-choi.com";
