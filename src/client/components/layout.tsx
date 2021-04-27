804b4482386221d9cee35c59efc6b3ca0a10c49d
koreanwglasses <koreanwglasses@gmail.com>
2021-04-27 17:04:34 -0500

    [Feature] [WIP] Refactored layout with material ui

--- src/client/components/layout.tsx
+++ src/client/components/layout.tsx
@@ -2,6 +2,7 @@ import {
   AppBar,
   Avatar,
   Button,
+  Container,
   createMuiTheme,
   CssBaseline,
   makeStyles,
@@ -9,18 +10,21 @@ import {
   Toolbar,
   Typography,
 } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { routes } from "../../endpoints";
 import { resolveEndpoint } from "../../utils";
 import { useAPI } from "../hooks/use-api";
-// import styles from "./layout.module.scss";
-// import "./layout.scss";
 import { UnstyledLink } from "./ui/unstyled-link";
 
 const theme = createMuiTheme({ palette: { type: "dark" } });
 
 const useStyles = makeStyles((theme) => ({
+  toolbar: {},
+  toolbarTitle: {
+    flexGrow: 1,
+  },
   button: {
     margin: theme.spacing(1, 1.5),
   },
@@ -30,53 +34,74 @@ export const Layout = ({ children }: React.PropsWithChildren<unknown>) => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <Header />
-    <main>{children}</main>
+    <Container>{children}</Container>
     <Footer />
   </ThemeProvider>
 );
 
 const Header = () => {
   const classes = useStyles();
-  const [err, user] = useAPI(routes.apiCurrentUser);
   return (
     <>
       <AppBar>
-        <Toolbar>
-          <UnstyledLink to={"/"}>
-            <Typography variant={"h3"}>
+        <Toolbar className={classes.toolbar}>
+          <Typography variant={"h4"} className={classes.toolbarTitle}>
+            <UnstyledLink to={"/"}>
               <b>CON</b>CORD
-            </Typography>
-          </UnstyledLink>
-
-          {!user && (
-            <Button
-              color="secondary"
-              variant="contained"
-              href={resolveEndpoint(routes.auth)}
-              className={classes.button}
-            >
-              Login with Discord
-            </Button>
-          )}
-          {user && (
-            <Button
-              color="secondary"
-              variant="contained"
-              component={RouterLink}
-              to={"/dashboard"}
-              className={classes.button}
-            >
-              Dashboard
-            </Button>
-          )}
-          {user && (
-            <Avatar
-              alt={`${user.username}#${user.discriminator}`}
-              src={user.avatarURL}
-            />
-          )}
+            </UnstyledLink>
+          </Typography>
+          <UserNav />
         </Toolbar>
       </AppBar>
+    </>
+  );
+};
+
+const UserNav = () => {
+  const classes = useStyles();
+  const [err, user] = useAPI(routes.apiCurrentUser);
+  return (
+    <>
+      {!err && !user && (
+        <>
+          <Skeleton
+            variant="rect"
+            width={120}
+            height={35}
+            className={classes.button}
+          />
+          <Skeleton variant="circle" width={40} height={40} />
+        </>
+      )}
+
+      {err && (
+        <Button
+          color="secondary"
+          variant="contained"
+          href={resolveEndpoint(routes.auth)}
+          className={classes.button}
+        >
+          Login with Discord
+        </Button>
+      )}
+
+      {user && (
+        <Button
+          color="secondary"
+          variant="contained"
+          component={RouterLink}
+          to={"/dashboard"}
+          className={classes.button}
+        >
+          Dashboard
+        </Button>
+      )}
+      {user && (
+        <Avatar
+          alt={`${user.username}#${user.discriminator}`}
+          src={user.avatarURL}
+        ></Avatar>
+      )}
     </>
   );
 };
