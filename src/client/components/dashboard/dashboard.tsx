1165350e943afb93869cdc9c52ba1aa5a82649e6
koreanwglasses <koreanwglasses@gmail.com>
2021-04-28 12:08:12 -0500

    [Update] improved visual of error page

--- src/client/components/dashboard/dashboard.tsx
+++ src/client/components/dashboard/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@material-ui/core";
+import { Box, Container, makeStyles } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import React from "react";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
@@ -10,18 +10,25 @@ import { ChannelDashboard } from "./channel-dashboard";
 import { GuildDashboard } from "./guild-dashboard";
 import { GuildSelector } from "./guild-selector";
 
+const useStyles = makeStyles((theme) => ({
+  contentContainer: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
+
 export const Dashboard = () => {
+  const classes = useStyles();
   const { path } = useRouteMatch();
   const [err] = useAPI(routes.apiCurrentUser);
   return (
     <Layout>
       {err?.isUnauthorized ? (
-        <>
+        <Container maxWidth="sm" className={classes.contentContainer}>
           <Alert severity="error">You must logged in to view this page</Alert>
           <Box m={2}>
             <LoginButton />
           </Box>
-        </>
+        </Container>
       ) : (
         <Switch>
           <Route exact path={path}>
