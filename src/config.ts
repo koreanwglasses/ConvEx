dad276ad6ff32b878e8d91dfff8adf00ed3f2045
koreanwglasses <koreanwglasses@gmail.com>
2021-05-02 02:08:50 -0500

    [update] Added production script

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
-export const baseURL = "http://localhost";
-export const port = 9000;
 export const mode =
   (process.env.NODE_ENV as
     | "production"
     | "development"
     | "remote-development") || "production";
-export const localFrontEndUrl = "http://localhost:8080";
+export const baseURL =
+  mode === "production" ? "http://dev.fred-choi.com" : "http://localhost";
+export const port = mode === "production" ? 80 : 9000;
+
 export const remoteBaseURL = "http://dev.fred-choi.com";
+export const localFrontEndUrl = "http://localhost:8080";
